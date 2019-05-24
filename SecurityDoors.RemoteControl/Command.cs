0e0dc2cef2fa0a7dc16949ef887919acfb16c293
Alexey Fedechkin <aleksey-fedechkin@rambler.ru>
2019-05-25 00:08:57 +0700

    add base logic of program

---
+++ SecurityDoors.RemoteControl/Command.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace SecurityDoors.RemoteControl
+{
+
+    class Command
+    {
+        public static bool addPerson()
+        {
+
+            throw new NotImplementedException();
+        }
+
+        internal static bool addDoor()
+        {
+            throw new NotImplementedException();
+        }
+
+        internal static bool addCard()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
