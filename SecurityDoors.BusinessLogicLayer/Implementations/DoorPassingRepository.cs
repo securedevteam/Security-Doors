42d5ee6d820c0c091c6e8ccee15650bbbd548318
Михаил Масный <masny.mikhail@gmail.com>
2019-05-24 19:49:11 +0300

    Добавлены в проект BLL пустые класс для работы с Repository.

---
+++ SecurityDoors.BusinessLogicLayer/Implementations/DoorPassingRepository.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SecurityDoors.BusinessLogicLayer.Implementations
+{
+    class DoorPassingRepository
+    {
+    }
+}
