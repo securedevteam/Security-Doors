e022fab818c56a4aebe3320aabdb424d33f7e872
Горелкин Александр <s207883@yandex.ru>
2019-05-24 12:08:57 +0300

    Реализован контекст приложения. Добавлен конфиг. файл. Добавлены миграции.

---
+++ SecurityDoors.DataAccessLayer/Migrations/20190524090535_FirstVersionOfDatabase.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SecurityDoors.DataAccessLayer.Migrations
+{
+    public partial class FirstVersionOfDatabase : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Doors",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    PersonEnter = table.Column<DateTime>(nullable: false),
+                    InOut = table.Column<bool>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Doors", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "People",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    FirstName = table.Column<string>(nullable: true),
+                    SecondName = table.Column<string>(nullable: true),
+                    LastName = table.Column<string>(nullable: true),
+                    Gender = table.Column<bool>(nullable: false),
+                    Passport = table.Column<string>(nullable: true),
+                    DoorId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_People", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_People_Doors_DoorId",
+                        column: x => x.DoorId,
+                        principalTable: "Doors",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Cards",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    UniqueNumber = table.Column<string>(nullable: true),
+                    Status = table.Column<bool>(nullable: false),
+                    PersonId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Cards", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Cards_People_PersonId",
+                        column: x => x.PersonId,
+                        principalTable: "People",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Cards_PersonId",
+                table: "Cards",
+                column: "PersonId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_People_DoorId",
+                table: "People",
+                column: "DoorId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Cards");
+
+            migrationBuilder.DropTable(
+                name: "People");
+
+            migrationBuilder.DropTable(
+                name: "Doors");
+        }
+    }
+}
---
+++ SecurityDoors.DataAccessLayer/Models/ApplicationContext.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+
+namespace SecurityDoors.DataAccessLayer.Models
+{
+	class ApplicationContext : DbContext
+	{
+		public DbSet<Person> People { get; set; }
+		public DbSet<Card> Cards { get; set; }
+		public DbSet<Door> Doors { get; set; }
+
+		/// <summary>
+		/// При отсутствии бд - создает пустую БД с нужной структурой
+		/// </summary>
+		public ApplicationContext()
+		{
+			Database.EnsureCreated();
+		}
+
+		/// <summary>
+		/// Устанавливает строку подключения из файла конфигураций к localDb
+		/// Вариации строк можно посмотреть здесь https://www.connectionstrings.com/sql-server/
+		/// </summary>
+		protected override void OnConfiguring (DbContextOptionsBuilder optionsBuilder)
+		{
+			var builder = new ConfigurationBuilder();
+			builder.SetBasePath(Directory.GetCurrentDirectory());
+			builder.AddJsonFile("appsettings.json");
+			var config = builder.Build();
+			string connectionString = config.GetConnectionString("DefaultConnection");
+
+			if (!optionsBuilder.IsConfigured)
+			{
+				optionsBuilder.UseSqlServer(connectionString);
+			}
+		}
+	}
+}
