4a2859bc3ea25cca8284e2ccfb469396828356ca
Михаил Масный <masny.mikhail@gmail.com>
2019-05-24 19:11:21 +0300

    Продправлен ряд файлов. Добавлены соответствующие проекты в решение.

--- SecurityDoors.App/Startup.cs
+++ SecurityDoors.App/Startup.cs
@@ -16,6 +16,15 @@ namespace SecurityDoors.App
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.Configure<CookiePolicyOptions>(options =>
+            {
+                // This lambda determines whether user consent for non-essential cookies 
+                // is needed for a given request.
+                options.CheckConsentNeeded = context => true;
+                options.MinimumSameSitePolicy = SameSiteMode.None;
+            });
+
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
--- SecurityDoors.DataAccessLayer/Models/ApplicationContext.cs
+++ SecurityDoors.DataAccessLayer/Models/ApplicationContext.cs
@@ -26,6 +26,8 @@ namespace SecurityDoors.DataAccessLayer.Models
 		/// </summary>
 		protected override void OnConfiguring (DbContextOptionsBuilder optionsBuilder)
 		{
+            // TODO: Удалить после приведение кода в порядок.
+
             //var builder = new ConfigurationBuilder();
             //builder.SetBasePath(Directory.GetCurrentDirectory());
             //builder.AddJsonFile("appsettings.json");
@@ -42,48 +44,6 @@ namespace SecurityDoors.DataAccessLayer.Models
         }
 		protected override void OnModelCreating(ModelBuilder modelBuilder)
 		{
-			modelBuilder.Entity<Card>().HasData(
-				new Card[]
-				{
-					new Card { Id = 1, UniqueNumber = Guid.NewGuid().ToString(), Status = true},
-					new Card { Id = 2, UniqueNumber = Guid.NewGuid().ToString(), Status = false},
-					new Card { Id = 3, UniqueNumber = Guid.NewGuid().ToString(), Status = false},
-					new Card { Id = 4, UniqueNumber = Guid.NewGuid().ToString(), Status = false},
-					new Card { Id = 5, UniqueNumber = Guid.NewGuid().ToString(), Status = true},
-					new Card { Id = 6, UniqueNumber = Guid.NewGuid().ToString(), Status = true},
-					new Card { Id = 7, UniqueNumber = Guid.NewGuid().ToString(), Status = false},
-					new Card { Id = 8, UniqueNumber = Guid.NewGuid().ToString(), Status = true},
-					new Card { Id = 9, UniqueNumber = Guid.NewGuid().ToString(), Status = false},
-					new Card { Id = 10, UniqueNumber = Guid.NewGuid().ToString(), Status = false},
-				}
-				);
-			modelBuilder.Entity<Person>().HasData(
-				new Person[]
-				{
-				new Person { Id=1, FirstName="Иван", SecondName = "Иванович", LastName = "Иванов", Gender = true, Passport = $"{new Random().Next(1000, 9000).ToString()} {new Random().Next(100000, 900000).ToString()}", CardId = 1},
-				new Person { Id=2, FirstName="Петр", SecondName = "Петрович", LastName = "Петров", Gender = true, Passport = $"{new Random().Next(1000, 9000).ToString()} {new Random().Next(100000, 900000).ToString()}", CardId = 5},
-				new Person { Id=3, FirstName="Михаил", SecondName = "Михайлович", LastName = "Михайлов", Gender = true, Passport = $"{new Random().Next(1000, 9000).ToString()} {new Random().Next(100000, 900000).ToString()}", CardId = 6},
-				new Person { Id=4, FirstName="Алексей", SecondName = "Алексеевич", LastName = "Алсексеев", Gender = true, Passport = $"{new Random().Next(1000, 9000).ToString()} {new Random().Next(100000, 900000).ToString()}", CardId = 8},
-				new Person { Id=5, FirstName="Юрий", SecondName = "Юрьевич", LastName = "Юрьев", Gender = true, Passport = $"{new Random().Next(1000, 9000).ToString()} {new Random().Next(100000, 900000).ToString()}", CardId = 10},
-				new Person { Id=6, FirstName="Василиса", SecondName = "Васильевна", LastName = "Вась", Gender = false, Passport = $"{new Random().Next(1000, 9000).ToString()} {new Random().Next(100000, 900000).ToString()}", CardId = 2},
-				});
-			modelBuilder.Entity<Door>().HasData(
-				new Door[]
-				{
-					new Door { Id = 1, Name = "Парадная", Description = "Основной вход"},
-					new Door { Id = 2, Name = "Вход для персонала", Description = "Основной вход для персонала"},
-				});
-			modelBuilder.Entity<DoorPassing>().HasData(
-				new DoorPassing[]
-				{
-					new DoorPassing { Id = 1, DoorId = 1, PersonId = 1 },
-					new DoorPassing { Id = 2, DoorId = 1, PersonId = 2},
-					new DoorPassing { Id = 3, DoorId = 1, PersonId = 3},
-					new DoorPassing { Id = 4, DoorId = 1, PersonId = 4},
-					new DoorPassing { Id = 5, DoorId = 2, PersonId = 5},
-					new DoorPassing { Id = 6, DoorId = 2, PersonId = 6}
-				});
-			base.OnModelCreating(modelBuilder);
 		}
 	}
 }
