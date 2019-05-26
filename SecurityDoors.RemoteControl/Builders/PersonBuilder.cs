484c8b9963318ec7c60d99c38b4e6a7f163e3709
Alexey Fedechkin <aleksey-fedechkin@rambler.ru>
2019-05-26 22:05:55 +0700

    Способ ввода вывода архетиктурно отделен от логики модуля

---
+++ SecurityDoors.RemoteControl/Builders/PersonBuilder.cs
@@ -0,0 +1,53 @@
+﻿using SecurityDoors.DataAccessLayer.Models;
+
+namespace SecurityDoors.RemoteControl
+{
+    class PersonBuilder
+    {
+        private Person person;
+        public PersonBuilder()
+        {
+            person = new Person();
+        }
+
+        public Person build()
+        {
+            return person;
+        }
+        public PersonBuilder setName(string name)
+        {
+            person.FirstName = name;
+            return this;
+        }
+
+        public PersonBuilder setSecondName(string secondName)
+        {
+            person.SecondName = secondName;
+            return this;
+        }
+
+        public PersonBuilder setLastName(string lastName)
+        {
+            person.LastName = lastName;
+            return this;
+        }
+
+        public PersonBuilder setGender(bool gender)
+        {
+            person.Gender = gender;
+            return this;
+        }
+
+        public PersonBuilder setPassport(string passport)
+        {
+            person.Passport = passport;
+            return this;
+        }
+
+        public PersonBuilder setCard(Card card)
+        {
+            person.Card = card;
+            return this;
+        }
+    }
+}
