aef78b33e1dc9f59b90daa99f6831a1ad7e2ead3
Михаил Масный <masny.mikhail@gmail.com>
2019-05-28 18:38:46 +0300

    Исправлена модель Door.

--- SecurityDoors.DataAccessLayer/Models/Door.cs
+++ SecurityDoors.DataAccessLayer/Models/Door.cs
@@ -1,26 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
+﻿using System.Collections.Generic;
 
 namespace SecurityDoors.DataAccessLayer.Models
 {
-	///TODO: Запретить NULL значения
 	/// <summary>
 	/// Модель двери.
 	/// </summary>
 	public class Door
     {
 		/// <summary>
-		/// Id дверного контроллера.
+		/// Id двери.
 		/// </summary>
 
 		public int Id { get; set; }
 
-		[Required]
+        /// <summary>
+        /// Название.
+        /// </summary>
 		public string Name { get; set; }
-
-		[Required]
+        
+        /// <summary>
+        /// Описание.
+        /// </summary>
 		public string Description { get; set; }
+
 		public virtual ICollection<DoorPassing> DoorPassings { get; set; }
 		public Door()
 		{
