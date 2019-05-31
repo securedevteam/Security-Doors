a2c361bea9cab58e668f60f53e746d3c8c060a39
Михаил Масный <masny.mikhail@gmail.com>
2019-05-31 20:59:41 +0300

    Восстановление рабочей версии решения. Смена 2.2 - > 2.1.*

--- SecurityDoors.App/Controllers/LoggerController.cs
+++ SecurityDoors.App/Controllers/LoggerController.cs
@@ -10,6 +10,7 @@ using System.Threading.Tasks;
 
 namespace SecurityDoors.App.Controllers
 {
+    [Route("api/[controller]")]
     public class LoggerController : Controller
     {
         private readonly ILoggerRepository _loggerRepository;
