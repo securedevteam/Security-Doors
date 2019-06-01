2f0c1a12af93e25818819daa77a6a167424b6da1
Михаил Масный <masny.mikhail@gmail.com>
2019-06-01 17:33:48 +0300

    Изменение модели. Добавление поле Status.

--- SecurityDoors.DataAccessLayer/Models/Door.cs
+++ SecurityDoors.DataAccessLayer/Models/Door.cs
@@ -22,10 +22,15 @@ namespace SecurityDoors.DataAccessLayer.Models
         /// </summary>
 		public string Description { get; set; }
 
-		/// <summary>
-		/// Комментарий.
+        /// <summary>
+		/// Статус.
 		/// </summary>
-		public string Comment { get; set; }
+		public int Status { get; set; }
+
+        /// <summary>
+        /// Комментарий.
+        /// </summary>
+        public string Comment { get; set; }
 
 		public virtual ICollection<DoorPassing> DoorPassings { get; set; }
 	}
