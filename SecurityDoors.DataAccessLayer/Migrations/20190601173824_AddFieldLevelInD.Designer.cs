39b53723a203ddb4bbfa72f72be40260f43ef712
Михаил Масный <masny.mikhail@gmail.com>
2019-06-01 20:38:34 +0300

    Выполнена миграция.

--- SecurityDoors.DataAccessLayer/Migrations/20190601173824_AddFieldLevelInD.Designer.cs
+++ SecurityDoors.DataAccessLayer/Migrations/20190601173824_AddFieldLevelInD.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SecurityDoors.DataAccessLayer.Models;
 
 namespace SecurityDoors.DataAccessLayer.Migrations
 {
     [DbContext(typeof(ApplicationContext))]
-    partial class ApplicationContextModelSnapshot : ModelSnapshot
+    [Migration("20190601173824_AddFieldLevelInD")]
+    partial class AddFieldLevelInD
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
