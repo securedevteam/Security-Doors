5983b813c8d91c96a204885f9b01370c2a1bcb0e
Михаил Масный <masny.mikhail@gmail.com>
2019-06-01 19:58:24 +0300

    Выполнена миграция.

--- SecurityDoors.DataAccessLayer/Migrations/20190601165722_AddFieldLocationInDP.Designer.cs
+++ SecurityDoors.DataAccessLayer/Migrations/20190601165722_AddFieldLocationInDP.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SecurityDoors.DataAccessLayer.Models;
 
 namespace SecurityDoors.DataAccessLayer.Migrations
 {
     [DbContext(typeof(ApplicationContext))]
-    partial class ApplicationContextModelSnapshot : ModelSnapshot
+    [Migration("20190601165722_AddFieldLocationInDP")]
+    partial class AddFieldLocationInDP
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
