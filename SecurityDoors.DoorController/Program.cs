8e4700c6b9aec25ddfc2dd803237f83a98b97d00
Михаил Масный <masny.mikhail@gmail.com>
2019-06-03 18:54:53 +0300

    Добавлены ссылки на вспомогательные проекты. Внедрен DI.

--- SecurityDoors.DoorController/Program.cs
+++ SecurityDoors.DoorController/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+using SecurityDoors.BusinessLogicLayer;
+using SecurityDoors.BusinessLogicLayer.Implementations;
+using SecurityDoors.BusinessLogicLayer.Interfaces;
 
 namespace SecurityDoors.DoorController
 {
@@ -6,7 +10,24 @@ namespace SecurityDoors.DoorController
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var serviceProvider = new ServiceCollection()    
+                .AddTransient<ICardRepository, CardRepository>()
+                .AddTransient<IDoorRepository, DoorRepository>()
+                .AddTransient<IDoorPassingRepository, DoorPassingRepository>()
+                .AddTransient<IPersonRepository, PersonRepository>()
+                .AddScoped<DataManager>()
+                .BuildServiceProvider();
+
+            var dataManagerService = serviceProvider.GetService<DataManager>();
+
+            var list = dataManagerService.Cards.GetCardsList();
+
+            foreach(var item in list)
+            {
+                Console.WriteLine($"{item.Id} {item.UniqueNumber} {item.Level} {item.Status}");
+            }
+
+            Console.ReadLine();
         }
     }
 }
