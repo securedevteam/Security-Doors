b81059b46eac03d572fb1e1aeff8a28a3254d37c
Михаил Масный <masny.mikhail@gmail.com>
2019-06-07 21:35:37 +0300

    Рефакторинг. Удален ненужный контекст.

--- SecurityDoors.Tests/CardRepositoryTests.cs
+++ SecurityDoors.Tests/CardRepositoryTests.cs
@@ -1,8 +1,6 @@
-﻿using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.Extensions.DependencyInjection;
 using SecurityDoors.BusinessLogicLayer;
 using SecurityDoors.DataAccessLayer.Models;
-using System;
 using System.Collections.Generic;
 using System.Linq;
 using Xunit;
