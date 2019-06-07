535d4f638589c46e041333bf57021e5ad0d5abe8
Михаил Масный <masny.mikhail@gmail.com>
2019-06-07 19:34:11 +0300

    Реализация примитивного теста.

---
+++ SecurityDoors.Tests/CardRepositoryTests.cs
@@ -0,0 +1,28 @@
+﻿using Xunit;
+
+namespace SecurityDoors.Tests
+{
+    /// <summary>
+    /// Класс для тестов класса CardRepository.
+    /// </summary>
+    public class CardRepositoryTests
+    {
+        /// <summary>
+        /// Примитивный тест.
+        /// </summary>
+        [Fact]
+        public void TestMethod1()
+        {
+            // Arrange
+            var a = 1;
+            var b = 2;
+            var expected = 3;
+
+            // Act
+            var actual = 3;
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+    }
+}
