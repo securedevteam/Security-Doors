dace830d601d2d819ea2622f96e8f0abfb606d56
Горелкин Александр <s207883@yandex.ru>
2019-06-10 18:49:47 +0300

    Некоторые правки в .travis.yml

--- SecurityDoors.Tests/PersonRepositoryTests.cs
+++ SecurityDoors.Tests/PersonRepositoryTests.cs
@@ -3,12 +3,13 @@ using SecurityDoors.BusinessLogicLayer;
 using SecurityDoors.DataAccessLayer.Models;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using Xunit;
 
 namespace SecurityDoors.Tests
 {
-	class PersonRepositoryTests : IClassFixture<ServiceFixture>, IDisposable
+	public class PersonRepositoryTests : IClassFixture<ServiceFixture>, IDisposable
 	{
 		private readonly ServiceProvider _serviceProvider;
 		private readonly DataManager _dataManagerService;
@@ -34,6 +35,94 @@ namespace SecurityDoors.Tests
 			}
 
 			_context.SaveChanges();
+		}
+
+		/// <summary>
+		/// Тест получения списка людей
+		/// </summary>
+		[Fact]
+		public void GetPeopleListTest ()
+		{
+			// Arrange
+			var people = new List<Person>();
+			var expected = 10;
+
+			for (int i = 0; i < expected; i++)
+			{
+				people.Add(new Person()
+				{
+					FirstName = Guid.NewGuid().ToString(),
+					SecondName = Guid.NewGuid().ToString(),
+					LastName = Guid.NewGuid().ToString(),
+					Comment = Guid.NewGuid().ToString(),
+					Passport = Guid.NewGuid().ToString(),
+					Gender = rnd.Next(0,1)
+				});
+			}
+
+			// Act
+			_context.People.AddRange(people);
+			_context.SaveChanges();
+
+			var peopleInDB = _dataManagerService.People.GetPeopleList().ToList();
+			var actual = peopleInDB.Count;
+
+			// Assert
+			Assert.Equal(expected, actual);
+			Assert.True(Equal(people, peopleInDB));
+		}
+
+		[Fact]
+		public void GetPersonByIdTest ()
+		{
+			var person = new Person()
+			{
+
+			};
+		}
+		/// <summary>
+		/// Сравнивает два объекта Person по каждому полю
+		/// </summary>
+		/// <param name="personA">Первый человек</param>
+		/// <param name="personB">Второй человек</param>
+		/// <returns>Если все поля совпадают - true</returns>
+		private static bool Equal (Person personA, Person personB)
+		{
+			return personA.Id == personB.Id &&
+					personA.FirstName == personB.FirstName &&
+					personA.SecondName == personB.SecondName &&
+					personA.LastName == personB.LastName &&
+					personA.Comment == personB.Comment &&
+					personA.CardId == personB.CardId &&
+					personA.Card == personB.Card &&
+					personA.Gender == personB.Gender &&
+					personA.Passport == personB.Passport;
+		}
+
+		/// <summary>
+		/// Сравнивает два списка людей по каждому полю их содержимого
+		/// </summary>
+		/// <param name="peopleA">Первый список</param>
+		/// <param name="peopleB">Второй список</param>
+		/// <returns>Если все поля всех людей совпадают - true</returns>
+		private static bool Equal (List<Person> peopleA, List<Person> peopleB)
+		{
+			if (peopleA.Count != peopleB.Count)
+			{
+				return false;
+			}
+			for (int i = 0; i < peopleA.Count; i++)
+			{
+				if (Equal(peopleA[i],peopleB[i]))
+				{
+					continue;
+				}
+				else
+				{
+					return false;
+				}
+			}
+			return true;
 		}
 	}
 }
