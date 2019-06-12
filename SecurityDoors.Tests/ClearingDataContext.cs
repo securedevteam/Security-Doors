07b0c15ef81c0fd3fa88cca4c76909908ebe274a
Михаил Масный <masny.mikhail@gmail.com>
2019-06-12 21:17:08 +0300

    Добавлен вспомогательный класс для упращения очистки контекста данных.

---
+++ SecurityDoors.Tests/ClearingDataContext.cs
@@ -0,0 +1,42 @@
+﻿using SecurityDoors.DataAccessLayer.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SecurityDoors.Tests
+{
+    public class ClearingDataContext
+    {
+        private readonly ApplicationContext _context;
+
+        public ClearingDataContext(ApplicationContext context)
+        {
+            _context = context;
+        }
+
+        public void Clear()
+        {
+            foreach (var entity in _context.Cards)
+            {
+                _context.Cards.Remove(entity);
+            }
+
+            foreach (var entity in _context.Doors)
+            {
+                _context.Doors.Remove(entity);
+            }
+
+            foreach (var entity in _context.DoorPassings)
+            {
+                _context.DoorPassings.Remove(entity);
+            }
+
+            foreach (var entity in _context.People)
+            {
+                _context.People.Remove(entity);
+            }
+
+            _context.SaveChanges();
+        }
+    }
+}
