f81c8a6886c4be24297e13668d31e78721535683
Михаил Масный <masny.mikhail@gmail.com>
2019-06-20 16:01:08 +0300

    Внесены изменения в метод ConfigureServices.

--- SecurityDoors.App/Startup.cs
+++ SecurityDoors.App/Startup.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
@@ -43,6 +44,9 @@ namespace SecurityDoors.App
 
             services.AddDbContext<ApplicationContext>(options =>
                 options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddIdentity<User, IdentityRole>()
+                .AddEntityFrameworkStores<ApplicationContext>();
 
             // Добавлен DI
             services.AddTransient<ICardRepository, CardRepository>();
