e22a0915169e9bca1b8f05aea00b832bba3521e8
Михаил Масный <masny.mikhail@gmail.com>
2019-06-20 15:58:27 +0300

    Расширена модель User на основе Identity.

---
+++ SecurityDoors.DataAccessLayer/Models/User.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace SecurityDoors.DataAccessLayer.Models
+{
+    /// <summary>
+    /// Расширение модели IdentityUser.
+    /// </summary>
+    public class User : IdentityUser
+    {
+        /// <summary>
+        /// Прозвище.
+        /// </summary>
+        public int Nickname { get; set; }
+    }
+}
