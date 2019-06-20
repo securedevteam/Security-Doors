5fe94faaf5f3a4dfa321604b6e0fb2d660ab5d26
Михаил Масный <masny.mikhail@gmail.com>
2019-06-20 16:14:25 +0300

    Добавлена модель Authorization.

---
+++ SecurityDoors.PresentationLayer/ViewModels/AuthorizationViewModel.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SecurityDoors.PresentationLayer.ViewModels
+{
+    /// <summary>
+    /// Модель формы авторизации.
+    /// </summary>
+    public class AuthorizationViewModel
+    {       
+        /// <summary>
+        /// Электронный адрес.
+        /// </summary>
+        [Required(ErrorMessage = "Неверный электронный адрес")]
+        [Display(Name = "Электронный адрес")]
+        public string Email { get; set; }
+
+        /// <summary>
+        /// Пароль.
+        /// </summary>
+        [Required(ErrorMessage = "Неверный пароль")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Пароль")]
+        public string Password { get; set; }
+
+        /// <summary>
+        /// Запомнить.
+        /// </summary>
+        [Display(Name = "Запомнить?")]
+        public bool RememberMe { get; set; }
+
+        /// <summary>
+        /// Вернуться по определенному адресу.
+        /// </summary>
+        public string ReturnUrl { get; set; }  
+    }
+}
