53de799986d6aa2546675ae552afc760d0955fa2
Михаил Масный <masny.mikhail@gmail.com>
2019-06-23 16:38:49 +0300

    Добавлен WebAPI контроллер для Cards. Добавлены XML комментарии. Измены WebAPI контроллеры согласно новой необходимой информации для передачи.

--- SecurityDoors.App/Controllers/API/CardsController.cs
+++ SecurityDoors.App/Controllers/API/CardsController.cs
@@ -12,11 +12,11 @@ using System.Threading.Tasks;
 namespace SecurityDoors.App.Controllers.API
 {
     /// <summary>
-    /// WebAPI контроллер для дверей.
+    /// WebAPI контроллер для карточек.
     /// </summary>
     [Route("api/[controller]")]
     [ApiController]
-    public class DoorsController : ControllerBase
+    public class CardsController : ControllerBase
     {
         private ServicesManager _serviceManager;
         private readonly ILogger _logger;
@@ -25,34 +25,34 @@ namespace SecurityDoors.App.Controllers.API
         /// Конструктор.
         /// </summary>
         /// <param name="dataManager">менеджер для работы с репозиторием дверей.</param>
-        public DoorsController(DataManager dataManager, ILogger<DoorController> logger)
+        public CardsController(DataManager dataManager, ILogger<DoorController> logger)
         {
             _serviceManager = new ServicesManager(dataManager);
             _logger = logger;
         }
 
-        // GET: api/Doors
+        // GET: api/Cards
         [HttpGet]
-        public async Task<IEnumerable<string>> GetDoors()
+        public async Task<IEnumerable<string>> GetCards()
         {
-            var doorsNames = new List<string>();
-            var models = await _serviceManager.Doors.GetDoorsAsync();
+            var cardsUniqueNumbers = new List<string>();
+            var models = await _serviceManager.Cards.GetCardsAsync();
 
             if (models == null || models.Count == 0)
             {
-                _logger.LogWarning(CommonUnsuccessfulEvents.ListItemsNotFound, DoorLoggerConstants.DOORS_LIST_IS_EMPTY);
+                _logger.LogWarning(CommonUnsuccessfulEvents.ListItemsNotFound, CardLoggerConstants.CARDS_LIST_IS_EMPTY);
             }
             else
             {
-                _logger.LogInformation(CommonSuccessfulEvents.ListItems, DoorLoggerConstants.DOORS_LIST_IS_NOT_EMPTY + models.Count + AppConstants.DOT);
+                _logger.LogInformation(CommonSuccessfulEvents.ListItems, CardLoggerConstants.CARDS_LIST_IS_NOT_EMPTY + models.Count + AppConstants.DOT);
             }
 
             foreach (var item in models)
             {
-                doorsNames.Add(item.Name);
+                cardsUniqueNumbers.Add(item.UniqueNumber);
             }
 
-            return doorsNames;
+            return cardsUniqueNumbers;
         }
     }
 }
