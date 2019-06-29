2990d54fbf818834f33aee71e0f9b64c0e79a15b
Михаил Масный <masny.mikhail@gmail.com>
2019-06-29 12:29:49 +0300

    Статические ошибки перенесены в соответствующую папку.

--- SecurityDoors.App/Startup.cs
+++ SecurityDoors.App/Startup.cs
@@ -80,7 +80,7 @@ namespace SecurityDoors.App
             app.UseSwagger();
             app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "SecurityDoors API version 1"));
             app.UseHttpsRedirection();
-            app.UseStatusCodePagesWithRedirects("/{0}.html");
+            app.UseStatusCodePagesWithRedirects("/Errors/{0}.html");
             app.UseStaticFiles();
             app.UseAuthentication();
             app.UseCookiePolicy();
