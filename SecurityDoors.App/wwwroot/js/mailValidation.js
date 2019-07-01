f1938985bf1421c2fa010bb6db9abcfd354d25fa
Горелкин Александр <s207883@yandex.ru>
2019-07-01 18:38:46 +0300

    Добавлен скрипт проверки введенного e-mail

---
+++ SecurityDoors.App/wwwroot/js/mailValidation.js
@@ -0,0 +1,15 @@
+﻿function ValidMail() {
+    var re = /^[\w-\.]+@[\w-]+\.[a-z]{2,4}$/i;
+    var myMail = document.getElementById('email').value;
+    var valid = re.test(myMail);
+    if (valid) {
+        document.getElementById('submit').disabled = false;
+        output = '';
+    }
+    else {
+        document.getElementById('submit').setAttribute('disabled', 'disabled');
+        output = 'Адрес электронной почты введен неправильно!';
+    }
+    document.getElementById('message').innerHTML = output;
+    return valid;
+}
