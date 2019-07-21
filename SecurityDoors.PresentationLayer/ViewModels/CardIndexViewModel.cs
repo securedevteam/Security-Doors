6000de82b8c17a1247f7f1b35a7dcd4dffb87143
Михаил Масный <masny.mikhail@gmail.com>
2019-07-21 21:03:06 +0300

    Добавлена стандартная постраничная навигация (пагинация) для Card Index.cshtml. Добавлены представления. Расширен контроллер.

---
+++ SecurityDoors.PresentationLayer/ViewModels/CardIndexViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace SecurityDoors.PresentationLayer.ViewModels
+{
+    public class CardIndexViewModel
+    {
+        public IEnumerable<CardViewModel> Cards { get; set; }
+        public PageViewModel PageViewModel { get; set; }
+    }
+}
