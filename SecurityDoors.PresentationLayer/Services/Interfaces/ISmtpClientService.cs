532036ae92790fa0cd58ee3aed5e0d400d1fee7a
Горелкин Александр <s207883@yandex.ru>
2019-07-22 15:59:49 +0300

    Добавлен интерфейс класса  SMTP клиента

---
+++ SecurityDoors.PresentationLayer/Services/Interfaces/ISmtpClientService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SecurityDoors.PresentationLayer.Services.Interfaces
+{
+	public interface ISmtpClientService
+	{
+		/// <summary>
+		/// Начинает асинхронную отправку сообщения
+		/// </summary>
+		/// <param name="to">e-mail получателя</param>
+		/// <param name="subject">Заголовок</param>
+		/// <param name="body">Содержимое письма</param>
+		/// <param name="attachment">Прикрепленные файлы</param>
+		Task SendEmailAsync(string to, string subject, string body, Attachment attachment = null);
+		
+		/// <summary>
+		/// Настроить smtp-клиент не из файла настроек
+		/// </summary>
+		/// <param name="senderEmail">e-mail отправителя</param>
+		/// <param name="password">Пароль</param>
+		/// <param name="smtpServerAddress">Адрес smtp сервера</param>
+		/// <param name="smtpServerPort">Порт smtp сервера</param>
+		/// <returns></returns>
+		Task ConfigureSmtpClientAsync(string senderEmail, string password, string smtpServerAddress, string smtpServerPort);
+	}
+}
