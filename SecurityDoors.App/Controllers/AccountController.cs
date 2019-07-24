54be66940414ee6f1c650a16a10200fdd957a22a
Горелкин Александр <s207883@yandex.ru>
2019-07-24 13:42:25 +0300

    Исправлено получение настроек из файла. Реализована отправка сообщений при регистрации.

--- SecurityDoors.App/Controllers/AccountController.cs
+++ SecurityDoors.App/Controllers/AccountController.cs
@@ -5,6 +5,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using SecurityDoors.BusinessLogicLayer;
 using SecurityDoors.Core.Constants;
+using SecurityDoors.Core.EmailService.Implementations;
 using SecurityDoors.Core.Logger.Constants;
 using SecurityDoors.Core.Logger.Events;
 using SecurityDoors.DataAccessLayer.Models;
@@ -161,22 +162,22 @@ namespace SecurityDoors.App.Controllers
             return NotFound();
         }
 
-        /// <summary>
-        /// Регистрация.
-        /// </summary>
-        /// <returns></returns>
-        [HttpGet]
+		/// <summary>
+		/// Регистрация.
+		/// </summary>
+		/// <returns>Представление.</returns>
+		[HttpGet]
         public IActionResult Registration()
         {
             return View();
         }
 
-        /// <summary>
-        /// Регистрация.
-        /// </summary>
-        /// <param name="model"></param>
-        /// <returns></returns>
-        [HttpPost]
+		/// <summary>
+		/// Регистрация.
+		/// </summary>
+		/// <param name="model"></param>
+		/// <returns>Представление.</returns>
+		[HttpPost]
         public async Task<IActionResult> Registration(RegistrationViewModel model)
         {
             if (ModelState.IsValid)
@@ -200,7 +201,17 @@ namespace SecurityDoors.App.Controllers
 
                         _logger.LogInformation(UserSuccessfulEvents.RegisterUserItem, UserLoggerConstants.USER_IS_REGISTER + " " + UserLoggerConstants.USER_IS_VALID + $"{model.Email}.");
 
-                        return RedirectToAction("Index", "Home");
+						
+						var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
+						var callbackUrl = Url.Action(
+							"ConfirmEmail",
+							"Account",
+							new { userId = user.Id, code },
+							protocol: HttpContext.Request.Scheme);
+						var emailService = new EmailService();
+						await emailService.SendEmailAsync(user.Email, "Подтверждение регистрации", $"Подтвердите регистрацию, перейдя по ссылке: <a href='{callbackUrl}'>link</a>");
+
+                        return View("SuccessRegistration");
                     }
                     else
                     {
@@ -221,12 +232,38 @@ namespace SecurityDoors.App.Controllers
             return View(model);
         }
 
-        /// <summary>
-        /// Вход в систему.
-        /// </summary>
-        /// <param name="returnUrl">возврат по определенному адресу.</param>
-        /// <returns></returns>
-        [HttpGet]
+		/// <summary>
+		/// Подтверждение e-mail при переходе по ссылке в письме
+		/// </summary>
+		/// <param name="userId">ID пользователя</param>
+		/// <param name="code">Код</param>
+		/// <returns></returns>
+		[HttpGet]
+		[AllowAnonymous]
+		public async Task<IActionResult> ConfirmEmail(string userId, string code)
+		{
+			if (userId == null || code == null)
+			{
+				return View("Error");
+			}
+			var user = await _userManager.FindByIdAsync(userId);
+			if (user == null)
+			{
+				return View("Error");
+			}
+			var result = await _userManager.ConfirmEmailAsync(user, code);
+			if (result.Succeeded)
+				return RedirectToAction("Index", "Home");
+			else
+				return View("Error");
+		}
+
+		/// <summary>
+		/// Вход в систему.
+		/// </summary>
+		/// <param name="returnUrl">возврат по определенному адресу.</param>
+		/// <returns></returns>
+		[HttpGet]
         public IActionResult Login(string returnUrl = null)
         {
             return View(new LoginViewModel { ReturnUrl = returnUrl });
