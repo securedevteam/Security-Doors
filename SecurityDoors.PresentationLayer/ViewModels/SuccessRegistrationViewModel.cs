8893edee55a8ae50e549eaea0d2f42bb6cf5ee47
Михаил Масный <masny.mikhail@gmail.com>
2019-07-25 00:06:26 +0300

    Добавлены XML комментария для ViewModel.

--- SecurityDoors.PresentationLayer/ViewModels/SuccessRegistrationViewModel.cs
+++ SecurityDoors.PresentationLayer/ViewModels/SuccessRegistrationViewModel.cs
@@ -1,7 +1,13 @@
 ﻿namespace SecurityDoors.PresentationLayer.ViewModels
 {
+    /// <summary>
+    /// Модель данных об успешной регистрации.
+    /// </summary>
     public class SuccessRegistrationViewModel
     {
+        /// <summary>
+        /// Сообщение.
+        /// </summary>
         public string Message { get; set; }
     }
 }
