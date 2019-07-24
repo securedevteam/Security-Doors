720dc2ac9d817e71e870ddeb296a89716436cd2d
Михаил Масный <masny.mikhail@gmail.com>
2019-07-24 21:43:04 +0300

    Добавлен класс для постраничной навигации на странице Index.cshtml типа Person.

---
+++ SecurityDoors.PresentationLayer/Paginations/PersonIndexViewModel.cs
@@ -0,0 +1,11 @@
+﻿using SecurityDoors.PresentationLayer.ViewModels;
+using System.Collections.Generic;
+
+namespace SecurityDoors.PresentationLayer.Paginations
+{
+    public class PersonIndexViewModel
+    {
+        public IEnumerable<PersonViewModel> People { get; set; }
+        public PageViewModel PageViewModel { get; set; }
+    }
+}
