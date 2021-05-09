c462d21f044785a2b41af03c8c9f06e7ff003553
Mikhail M <47109585+MikhailMasny@users.noreply.github.com>
2021-05-09 16:28:03 +0300

    feat: implement DatabaseSeed project (console app)

---
+++ src/Secure.SecurityDoors.DatabaseSeed/Program.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Secure.SecurityDoors.Data.Contexts;
+using Secure.SecurityDoors.Data.Models;
+using Secure.SecurityDoors.DatabaseSeed.Interfaces;
+using Secure.SecurityDoors.DatabaseSeed.Services;
+using System;
+
+namespace Secure.SecurityDoors.DatabaseSeed
+{
+    // TODO: use resource files
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            var creationService = GetServiceProvider()
+                .GetService<ICreationService>();
+
+            if (Question("role"))
+            {
+                creationService.CreateRole()
+                    .GetAwaiter()
+                    .GetResult();
+            }
+
+            Console.WriteLine();
+
+            if (Question("user"))
+            {
+                creationService.CreateUser()
+                    .GetAwaiter()
+                    .GetResult();
+            }
+
+            Console.ReadKey();
+        }
+
+        private static bool Question(string model)
+        {
+            Console.WriteLine($"It is necessary to create {model}? Y/N");
+            var userInput = Console.ReadLine();
+            return !string.IsNullOrWhiteSpace(userInput) && userInput.ToLower() != "n";
+        }
+
+        private static ServiceProvider GetServiceProvider()
+        {
+            const string connectionString =
+                "Server=(localdb)\\mssqllocaldb;Database=SecurityDoorsApp;Trusted_Connection=True;";
+
+            var services = new ServiceCollection()
+                .AddDbContext<ApplicationContext>(options =>
+                    options.UseSqlServer(connectionString))
+                .AddSingleton<ILoggerFactory, LoggerFactory>()
+                .AddSingleton(typeof(ILogger<>), typeof(Logger<>))
+                .AddScoped<ICreationService, CreationService>();
+
+            services.AddIdentity<User, IdentityRole>()
+                .AddEntityFrameworkStores<ApplicationContext>();
+
+            return services.BuildServiceProvider();
+        }
+    }
+}
---
+++ src/Secure.SecurityDoors.DatabaseSeed/Services/CreationService.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Secure.SecurityDoors.Data.Enums;
+using Secure.SecurityDoors.Data.Models;
+using Secure.SecurityDoors.DatabaseSeed.Interfaces;
+using System;
+using System.Threading.Tasks;
+
+namespace Secure.SecurityDoors.DatabaseSeed.Services
+{
+    public class CreationService : ICreationService
+    {
+        public readonly UserManager<User> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        public CreationService(
+            UserManager<User> userManager,
+            RoleManager<IdentityRole> roleManager)
+        {
+            _userManager = userManager ?? throw new ArgumentNullException(nameof(userManager));
+            _roleManager = roleManager ?? throw new ArgumentNullException(nameof(roleManager));
+        }
+
+        public async Task CreateRole()
+        {
+            var role = ConsoleInput("role");
+            if (!await _roleManager.RoleExistsAsync(role))
+            {
+                await _roleManager.CreateAsync(new IdentityRole(role));
+                Console.WriteLine("Done.");
+            }
+            else
+            {
+                Console.WriteLine("Role already exist.");
+            }
+        }
+
+        public async Task CreateUser()
+        {
+            var email = ConsoleInput("email");
+            var userName = ConsoleInput("username");
+            var firstName = ConsoleInput("first name");
+            var lastName = ConsoleInput("last name");
+            var secondName = ConsoleInput("second name");
+            var gender = ConsoleInput("gender");
+            var passport = ConsoleInput("passport");
+            var identificationNumber = ConsoleInput("identification number");
+            var comment = ConsoleInput("comment");
+            var password = ConsoleInput("password");
+            var role = ConsoleInput("role");
+
+            var user = new User
+            {
+                Email = email,
+                UserName = userName,
+                FirstName = firstName,
+                LastName = lastName,
+                SecondName = secondName,
+                Gender = Enum.Parse<GenderType>(gender),
+                Passport = passport,
+                IdentificationNumber = identificationNumber,
+                Comment = comment,
+            };
+
+            var result = await _userManager.CreateAsync(user, password);
+            if (!result.Succeeded)
+            {
+                foreach (var error in result.Errors)
+                {
+                    Console.WriteLine(error.Description);
+                }
+            }
+            else
+            {
+                await _userManager.AddToRoleAsync(user, role);
+
+                Console.WriteLine("Done.");
+            }
+        }
+
+        private static string ConsoleInput(string type)
+        {
+            Console.Write($"Enter {type}: ");
+            return Console.ReadLine();
+        }
+    }
+}
